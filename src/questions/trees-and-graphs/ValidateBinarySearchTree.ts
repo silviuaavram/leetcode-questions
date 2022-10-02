b0f07e783ada16f5554fa1688b2562d19484e0c9
Silviu Alexandru Avram <silviu.avram91@gmail.com>
2022-10-02 18:02:11 +0300

    feat: ValidateBinarySearchTree

---
+++ src/questions/trees-and-graphs/ValidateBinarySearchTree.ts
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+import {TreeNode} from '../../types'
+
+export function isValidBST(root: TreeNode | null): boolean {
+  if (!root) {
+    return true
+  }
+
+  const traverseResult: number[] = []
+  const visitedNodes: Set<TreeNode> = new Set()
+  const stack: TreeNode[] = [root]
+
+  while (stack.length) {
+    const node = stack[stack.length - 1]
+
+    if (node.left && !visitedNodes.has(node.left)) {
+      stack.push(node.left)
+
+      continue
+    }
+
+    stack.pop()
+    traverseResult.push(node.val)
+    visitedNodes.add(node)
+
+    if (node.right && !visitedNodes.has(node.right)) {
+      stack.push(node.right)
+
+      continue
+    }
+  }
+
+  for (let index = 0; index < traverseResult.length; index++) {
+    const previousValue = traverseResult[index - 1]
+    const currentValue = traverseResult[index]
+    const nextValue = traverseResult[index + 1]
+
+    if (previousValue !== undefined && previousValue >= currentValue) {
+      return false
+    }
+
+    if (nextValue !== undefined && nextValue <= currentValue) {
+      return false
+    }
+  }
+
+  return true
+}
---
+++ src/questions/trees-and-graphs/__tests__/ValidateBinarySearchTree.test.ts
@@ -0,0 +1,30 @@
+import {buildTree} from '../../../utils'
+import {isValidBST} from '../ValidateBinarySearchTree'
+
+test('empty tree is BST', function emptyIsBST() {
+  const tree = buildTree([])
+  const isBST = isValidBST(tree)
+
+  expect(isBST).toBe(true)
+})
+
+test('is a BST', function validBST() {
+  const tree = buildTree([2, 1, 3])
+  const isBST = isValidBST(tree)
+
+  expect(isBST).toBe(true)
+})
+
+test('is not a BST', function validBST() {
+  const tree = buildTree([5, 1, 4, null, null, 3, 6])
+  const isBST = isValidBST(tree)
+
+  expect(isBST).toBe(false)
+})
+
+test('is not a BST if node values are equal', function validBST() {
+  const tree = buildTree([2, 2, 2])
+  const isBST = isValidBST(tree)
+
+  expect(isBST).toBe(false)
+})
