8cf32be480a85f16278612b507acd81eef7c91e3
Silviu Alexandru Avram <silviu.avram91@gmail.com>
2022-10-07 10:12:01 +0300

    feat: IntersectionOfTwoLinkedLists

---
+++ src/questions/linked-lists/IntersectionOfTwoLinkedLists.ts
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+import {ListNode} from '../../types'
+
+export function getIntersectionNode(
+  headA: ListNode | null,
+  headB: ListNode | null,
+): ListNode | null {
+  let listASize = 0
+  let listBSize = 0
+
+  let currentNode = headA
+
+  while (currentNode) {
+    listASize++
+    currentNode = currentNode.next
+  }
+
+  currentNode = headB
+
+  while (currentNode) {
+    listBSize++
+    currentNode = currentNode.next
+  }
+
+  let currentA = headA
+  let currentB = headB
+  const sizeDifference = Math.abs(listASize - listBSize)
+
+  if (listASize > listBSize) {
+    for (let index = 0; index < sizeDifference; index++) {
+      currentA = currentA.next
+    }
+  } else if (listBSize > listASize) {
+    for (let index = 0; index < sizeDifference; index++) {
+      currentB = currentB.next
+    }
+  }
+
+  while (currentA && currentB) {
+    if (currentA === currentB) {
+      return currentA
+    }
+
+    currentA = currentA.next
+    currentB = currentB.next
+  }
+
+  return null
+}
---
+++ src/questions/linked-lists/__tests__/IntersectionOfTwoLinkedLists.test.ts
@@ -0,0 +1,42 @@
+import {buildList} from '../../../utils'
+import {getIntersectionNode} from '../IntersectionOfTwoLinkedLists'
+
+test('lists intersect at node 8', function intersectAt8() {
+  const listA = buildList([4, 1, 8, 4, 5])
+  const listB = buildList([5, 6, 1])
+
+  listB.next.next.next = listA.next.next
+
+  expect(getIntersectionNode(listA, listB)).toEqual({
+    val: 8,
+    next: expect.any(Object),
+  })
+})
+test('lists intersect at node 2', function intersectAt2() {
+  const listA = buildList([1, 9, 1, 2, 4])
+  const listB = buildList([3])
+
+  listB.next = listA.next.next.next
+
+  expect(getIntersectionNode(listA, listB)).toEqual({
+    val: 2,
+    next: expect.any(Object),
+  })
+})
+
+test('lists do not intersect', function doNotIntersect() {
+  const listA = buildList([2, 6, 4])
+  const listB = buildList([1, 5])
+
+  expect(getIntersectionNode(listA, listB)).toBeNull()
+})
+
+test('list does not intersect with null', function doNotIntersectWithNull() {
+  const listA = buildList([2, 6, 4])
+
+  expect(getIntersectionNode(listA, null)).toBeNull()
+})
+
+test('nulls do not intersect', function nullsDoNotIntersect() {
+  expect(getIntersectionNode(null, null)).toBeNull()
+})
