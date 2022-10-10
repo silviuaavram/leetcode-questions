858bc0458a946668af8047ab4f45e7a59467ad68
Silviu Alexandru Avram <silviu.avram91@gmail.com>
2022-10-10 10:44:19 +0300

    feat: ReverseLinkedList

---
+++ src/questions/linked-lists/ReverseLinkedList.ts
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+import {ListNode} from '../../types'
+
+export function reverseList(head: ListNode | null): ListNode | null {
+  if (!head) {
+    return null
+  }
+
+  function reverseNode(node: ListNode, previous: ListNode = null): ListNode {
+    const next = node.next
+    node.next = previous
+
+    if (!next) {
+      return node
+    } else {
+      return reverseNode(next, node)
+    }
+  }
+
+  return reverseNode(head)
+}
---
+++ src/questions/linked-lists/__tests__/ReverseLinkedList.test.ts
@@ -0,0 +1,41 @@
+import {buildList} from '../../../utils'
+import {reverseList} from '../ReverseLinkedList'
+
+test('reverses a 5 node list', function reverse5NodeList() {
+  const values = [1, 2, 3, 4, 5]
+  const head = buildList(values)
+
+  const reversed = reverseList(head)
+
+  expect(reversed).toEqual(buildList(values.reverse()))
+})
+
+test('reverses a 2 node list', function reverse2NodeList() {
+  const values = [1, 2]
+  const head = buildList(values)
+
+  const reversed = reverseList(head)
+
+  expect(reversed).toEqual(buildList(values.reverse()))
+})
+
+test('reverses a 1 node list', function reverse1NodeList() {
+  const values = [1]
+  const head = buildList(values)
+
+  const reversed = reverseList(head)
+
+  expect(reversed).toEqual(buildList(values.reverse()))
+})
+
+test('reverse an empty list', function reverseEmptyList() {
+  let failed = false
+
+  try {
+    reverseList(null)
+  } catch (e) {
+    failed = true
+  }
+
+  expect(failed).toBe(false)
+})
