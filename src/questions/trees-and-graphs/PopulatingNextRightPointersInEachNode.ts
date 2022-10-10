50ec8145f966ac0bfcc777a15794462754e81f70
Silviu Alexandru Avram <silviu.avram91@gmail.com>
2022-10-10 10:26:35 +0300

    feat: PopulatingNextRightPointersInEachNode

---
+++ src/questions/trees-and-graphs/PopulatingNextRightPointersInEachNode.ts
@@ -0,0 +1,59 @@
+/**
+ * Definition for Node.
+ * class Node {
+ *     val: number
+ *     left: Node | null
+ *     right: Node | null
+ *     next: Node | null
+ *     constructor(val?: number, left?: Node, right?: Node, next?: Node) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+import {TreeNode} from '../../types'
+
+export type Node = TreeNode & {next: Node}
+
+export function connect(root: Node | null): Node | null {
+  if (!root) {
+    return null
+  }
+
+  const queue = [root]
+  const nextLevelQueue = []
+  const result = [[root]]
+
+  while (queue.length) {
+    const currentNode = queue.splice(0, 1)[0]
+
+    if (currentNode.left) {
+      nextLevelQueue.push(currentNode.left)
+    }
+    if (currentNode.right) {
+      nextLevelQueue.push(currentNode.right)
+    }
+
+    if (!queue.length && nextLevelQueue.length) {
+      queue.push(...nextLevelQueue)
+      result.push([...nextLevelQueue])
+      nextLevelQueue.length = 0
+    }
+  }
+
+  for (let levelIndex = 0; levelIndex < result.length; levelIndex++) {
+    for (
+      let nodeIndex = 0;
+      nodeIndex < result[levelIndex].length;
+      nodeIndex++
+    ) {
+      result[levelIndex][nodeIndex].next =
+        result[levelIndex][nodeIndex + 1] ?? null
+    }
+  }
+
+  return root
+}
---
+++ src/questions/trees-and-graphs/__tests__/PopulatingNextRightPointersInEachNode.test.ts
@@ -0,0 +1,38 @@
+import {buildTree} from '../../../utils'
+import {connect, Node} from '../PopulatingNextRightPointersInEachNode'
+
+test('connects the nodes correctly', function connectNodes() {
+  const tree = buildTree([1, 2, 3, 4, 5, 6, 7]) as Node
+
+  connect(tree)
+
+  expect(tree.next).toBeNull()
+
+  expect((tree.left as Node).next).toBe(tree.right)
+  expect((tree.right as Node).next).toBeNull()
+
+  expect((tree.left.left as Node).next).toBe(tree.left.right)
+  expect((tree.left.right as Node).next).toBe(tree.right.left)
+  expect((tree.right.left as Node).next).toBe(tree.right.right)
+  expect((tree.right.right as Node).next).toBeNull()
+})
+
+test('does not connect an empty tree', function emptyTree() {
+  let failed = false
+
+  try {
+    connect(null)
+  } catch (e) {
+    failed = true
+  }
+
+  expect(failed).toBe(false)
+})
+
+test('does connect only one node', function rootTreeConnect() {
+  const tree = buildTree([1]) as Node
+
+  connect(tree)
+
+  expect(tree.next).toBeNull()
+})
