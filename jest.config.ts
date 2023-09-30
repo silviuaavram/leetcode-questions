36c5b712451589cb3c2119ff8fca29f9afb68aac
Silviu Alexandu Avram <silviu.avram91@gmail.com>
2023-09-30 15:02:20 +0300

    fix jest aliases

--- jest.config.ts
+++ jest.config.ts
@@ -89,8 +89,8 @@ export default {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    '@utils/(.*)': '<rootDir>/src/utils/$1',
-    '@typings/(.*)': '<rootDir>/src/typings/$1',
+    '@utils': '<rootDir>/src/utils',
+    '@typings': '<rootDir>/src/typings',
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
