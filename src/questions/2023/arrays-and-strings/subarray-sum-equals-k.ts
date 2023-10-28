6e106de8334913c6190d701b1c2121fc726cc5ed
Silviu Alexandu Avram <silviu.avram91@gmail.com>
2023-10-28 19:51:49 +0300

    explanation for subarray sum equals k

--- src/questions/2023/arrays-and-strings/subarray-sum-equals-k.ts
+++ src/questions/2023/arrays-and-strings/subarray-sum-equals-k.ts
@@ -5,15 +5,20 @@ function subarraySum(nums: number[], k: number): number {
   let result = 0
   const sums: number[] = []
 
+  // store the sums from 0 to index in a separate array.
   for (let index = 0; index < nums.length; index++) {
     sums[index] = (sums[index - 1] ?? 0) + nums[index]
   }
 
+  // we check the sums
   for (let index1 = 0; index1 < nums.length; index1++) {
+    // if the sum until index1 is k, increment result
     if (sums[index1] === k) {
         result++
     }
 
+    // take all the sums until index1, subtract each from current sum and check if it's k
+    // these subtractions are basically the subarray sums from 0->index1, 1->index2, 2->index2 ...
     for (let index2 = 0; index2 < index1; index2++) {
         const sum = sums[index1] - sums[index2]
 
