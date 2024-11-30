434d44e09ebcd50f10c77288077dd558538e8390
Silviu Alexandu Avram <silviu.avram91@gmail.com>
2024-11-30 15:08:25 +0200

    comment

--- src/questions/2024-part-2/strings/valid-palindrome.ts
+++ src/questions/2024-part-2/strings/valid-palindrome.ts
@@ -1,11 +1,15 @@
 /**
  * https://leetcode.com/problems/valid-palindrome/description/
  *
- *
+ * We remove non alphanumeric chars, lowercase the rest and split into chars.
+ * Then we check char by char.
  */
 
 function isPalindrome(s: string): boolean {
-  const tokens = s.replace(/[^0-9a-z]/gi, '').toLowerCase().split('')
+  const tokens = s
+    .replace(/[^0-9a-z]/gi, '')
+    .toLowerCase()
+    .split('')
 
   for (let index = 0; index < tokens.length; index++) {
     if (tokens[index] !== tokens[tokens.length - 1 - index]) {
