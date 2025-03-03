899c956cbba76521fbe63a843b3e650102757294
Silviu Alexandu Avram <silviu.avram91@gmail.com>
2025-03-03 21:29:36 +0200

    powx-n

--- src/questions/2024-part-2/math/powx-n.ts
+++ src/questions/2024-part-2/math/powx-n.ts
@@ -1,33 +1,15 @@
 /**
  * https://leetcode.com/problems/powx-n/description
  *
- * This implementation passes the timed tests because of the binary exponentiation is faster
- * than my initial implementation below.
+ * We use recursive calls and memoisation.
+ * Recursion stops when n is 0 or 1.
+ * Otherwise, we return multiplication.
+ * Ex: 5^7 = 5^3 * 5^4.
+ * We save the results for different indeces in a map, so we don't re-calculate.
  */
 
 function myPow(x: number, n: number): number {
-  function getPow(x: number, n: number): number {
-    if (n === 0) {
-      return 1
-    }
-
-    if (n < 0) {
-      return 1 / getPow(x, -n)
-    }
-
-    // binary exponentiation. f(2,100) = f(2^2,50) = f(2^4,25) = 2^4 * f(2^4, 24) = ...
-    if (n % 2 === 1) {
-      return x * getPow(x * x, (n - 1) / 2)
-    } else {
-      return getPow(x * x, n / 2)
-    }
-  }
-
-  return Math.round(getPow(x, n) * 100000) / 100000
-}
-
-// My initial implementation using recursion exceeds time limit
-function myPow2(x: number, n: number): number {
+  const results = new Map<number, number>()
   function getPow(x: number, n: number): number {
     if (n < 0) {
       return 1 / getPow(x, -n)
@@ -41,7 +23,15 @@ function myPow2(x: number, n: number): number {
       return x
     }
 
-    return getPow(x, Math.floor(n / 2)) * getPow(x, Math.ceil(n / 2))
+    if (results.has(n)) {
+      return results.get(n)
+    }
+
+    const result = getPow(x, Math.floor(n / 2)) * getPow(x, Math.ceil(n / 2))
+
+    results.set(n, result)
+
+    return result
   }
 
   return Math.round(getPow(x, n) * 100000) / 100000
